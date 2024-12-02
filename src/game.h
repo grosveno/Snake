c724f2f0748a60ddab812017853d74bc34a1dcc1
yan <647979893@qq.com>
2024-12-02 16:42:15 +0800

    update

--- src/game.h
+++ src/game.h
@@ -9,6 +9,7 @@ void read_name(char* write_into);
 void read_status(char* write_into);
 void read_board(char* write_info);
 void read_txt(char* filename, char** board);
+void output_rank();
 void update(int* cells, size_t width, size_t height, snake_t* snake_p,
             enum input_key input, int growing);
 void place_food(int* cells, size_t width, size_t height);
